c2bc509c1da9e0250301f616de53b7e8092e2e68
Cynthia97 <872250335@qq.com>
2017-04-26 19:16:36 +0800

    Add files via upload

---
+++ Square4View.java
@@ -0,0 +1,167 @@
+package com.example.ssudoku;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Paint.Align;
+import android.graphics.Paint.FontMetrics;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class Square4View extends View{
+
+	public Square4View(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	private Game4 game=new Game4();
+	private float width;
+	private float hight;
+	private static int selectX;
+	private static int selectY;
+	private Rect selectRect=new Rect();
+	private Rect duplicatetRect=new Rect();
+	static boolean isFill;
+	
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		
+		this.width =( w-26) / 4f;
+		this.hight = ( w-26) / 4f;
+		
+		super.onSizeChanged(w, h, oldw, oldh);
+	}
+	 
+	 public void onDraw(Canvas canvas){
+		 super.onDraw(canvas);
+		 
+		 //width=((getWidth()-26)/9);
+		 //hight=((getHeight()-240)/9);
+		 
+		 //draw square
+		 Paint background = new Paint();
+		 background.setColor(Color.WHITE);
+		 
+		 Paint broaderline = new Paint();
+		 broaderline.setColor(Color.BLACK);
+		 broaderline.setStrokeWidth(3);
+		 broaderline.setStyle(Paint.Style.STROKE); 
+		 canvas.drawRect(13, 10, getWidth()-13, getWidth()-26, broaderline);
+		 
+		 Paint lightline = new Paint();
+		 lightline.setColor(Color.BLACK);
+		 Paint darkline = new Paint();
+		 darkline.setColor(Color.BLACK);
+		 darkline.setStrokeWidth(3);
+		 
+		 for(int i=1;i<4;i++){
+			 canvas.drawLine(13, 10+i*hight, getWidth()-13, 10+i*hight, lightline);
+			 canvas.drawLine(13+i*width, 10, 13+i*width , getWidth()-26, lightline);
+			 
+		 }
+		 
+		 for(int j=0;j<4;j++){
+			 if(j%2==0){
+				 canvas.drawLine(13, 10+j*hight, getWidth()-13, 10+j*hight, darkline);
+				 canvas.drawLine(13+j*width, 10, 13+j*width , getWidth()-26, darkline); 
+			 }
+		 }
+		 
+		//draw initial numbers
+		 Paint number=new Paint();
+		 number.setColor(Color.BLACK);
+		 number.setTextSize(hight/2);
+		 number.setTextAlign(Align.CENTER);
+		 FontMetrics fm = number.getFontMetrics();
+		 float x=13+width/2;
+		 float y=10+hight/2 - (fm.ascent+fm.descent)/2;
+		 
+		 for(int i=0;i<4;i++){
+			for(int j=0;j<4;j++){
+				canvas.drawText(game.getString(i,j),i*width+x,j*hight+y,number);
+			}
+		 }
+		 
+		 //draw selected area
+		 Paint selected = new Paint();
+	 	 selected.setColor(Color.CYAN);
+	 	 selected.setStrokeWidth(6);
+	 	 selected.setStyle(Paint.Style.STROKE); 
+	 	 if(!game.isOriginal(selectX, selectY)){
+	 		  canvas.drawRect(selectRect, selected);
+	 	 }
+	 	 
+	 	 //showNumber(selectX, selectY);
+	 	 //to check if it is duplicate
+		 Paint errorline = new Paint();
+		 errorline.setColor(Color.RED);
+		 errorline.setStrokeWidth(6);
+		 errorline.setStyle(Paint.Style.STROKE); 
+	 	 if(isFill==true){
+	 		if(game.isDuplicate4(selectX, selectY,game.j)){
+	 			if(!game.isOriginal(selectX, selectY)){
+	 				canvas.drawRect(duplicatetRect, errorline);
+					Log.d(" ", "false");
+	 			}
+		 	 }
+		 	 else {
+		 		Log.d(" ", "t");
+			}
+	 	 }
+	 	 
+	 	 if(game.isFinished()){
+	 		 if(game.isRight()){
+	 			 
+	 		 }
+	 	 }
+	 	 
+}
+	 
+		public boolean onTouchEvent(MotionEvent event) {
+			float clickX=event.getX();
+			float clickY=event.getY();
+			selectX = (int) ((clickX-13)/width);
+			selectY = (int) ((clickY-10)/width);
+				 
+			if (event.getAction()!= MotionEvent.ACTION_DOWN)
+				return super.onTouchEvent(event);	
+				 
+			Log.d("hhh", "aa");//to check if click successfully		 
+			if(clickY>10&&clickY<getWidth()-16)
+				selectedR(selectX, selectY);
+			duplicateR(selectX, selectY);
+//			initiaNum(selectX,selectY);
+//			showNumber(selectX, selectY);
+			isFill=game.fill_in_blank(game.j, selectY, selectX);
+			 		 
+			return true;
+			}
+			
+//		private void initiaNum(int x, int y) {
+//			game.getNumber("0");
+//			game.setNum(x, y);
+//			invalidate();
+//		}
+
+		public void selectedR(int x,int y){
+			invalidate(selectRect);
+			selectRect.set((int)(13+x*width), (int)(10+y*hight), (int)(13+(x+1)*width),(int)(13+(y+1)*hight));
+			invalidate(selectRect);
+			}
+			
+		public void duplicateR(int x,int y){
+			
+			duplicatetRect.set((int)(13+x*width), (int)(10+y*hight), (int)(13+(x+1)*width),(int)(13+(y+1)*hight));
+			invalidate(duplicatetRect);
+		}
+//			public void showNumber(int x,int y){
+//				game.setNum(x,y);
+//				invalidate();
+//							
+//			}
+
+}
---
+++ Square9View.java
@@ -0,0 +1,240 @@
+package com.example.ssudoku;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Paint.Align;
+import android.graphics.Paint.FontMetrics;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+
+public class Square9View extends View{
+
+	public Square9View(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Square9View(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}	
+	
+	
+	private Game game=new Game();
+	private float width;
+	private float hight;
+	private static int selectX;
+	private static int selectY;
+	private Rect selectRect=new Rect();
+	private Rect duplicatetRect=new Rect();
+	static boolean isFill;
+	
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		
+		this.width =( w-26) / 9f;
+		this.hight = ( w-26) / 9f;
+		
+		super.onSizeChanged(w, h, oldw, oldh);
+		System.out.print("hhhh");
+	}
+	 
+	 public void onDraw(Canvas canvas){
+		 
+		 
+		 //width=((getWidth()-26)/9);
+		 //hight=((getHeight()-240)/9);
+		 
+		 //draw square
+		 Paint background = new Paint();
+		 background.setColor(Color.WHITE);
+		 
+		 Paint broaderline = new Paint();
+		 broaderline.setColor(Color.BLACK);
+		 broaderline.setStrokeWidth(3);
+		 broaderline.setStyle(Paint.Style.STROKE); 
+		 canvas.drawRect(13, 10, getWidth()-13, getWidth()-16, broaderline);
+		 
+		 Paint lightline = new Paint();
+		 lightline.setColor(Color.BLACK);
+		 Paint darkline = new Paint();
+		 darkline.setColor(Color.BLACK);
+		 darkline.setStrokeWidth(3);
+		 
+		 //draw line
+		 for(int i=1;i<9;i++){
+			 canvas.drawLine(13, 10+i*hight, getWidth()-13, 10+i*hight, lightline);
+			 canvas.drawLine(13+i*width, 10, 13+i*width , getWidth()-16, lightline);
+			 
+		 }
+		 
+		 for(int j=0;j<9;j++){
+			 if(j%3==0){
+				 canvas.drawLine(13, 10+j*hight, getWidth()-13, 10+j*hight, darkline);
+				 canvas.drawLine(13+j*width, 10, 13+j*width , getWidth()-16, darkline); 
+			 }
+		 }
+		
+		 //draw initial numbers
+		 Paint number=new Paint();
+		 number.setColor(Color.BLACK);
+		 number.setTextSize(hight/2);
+		 number.setTextAlign(Align.CENTER);
+		 FontMetrics fm = number.getFontMetrics();
+		 float x=13+width/2;
+		 float y=10+hight/2 - (fm.ascent+fm.descent)/2;
+		 
+		 for(int i=0;i<9;i++){
+			for(int j=0;j<9;j++){
+				canvas.drawText(game.getString(i, j),i*width+x,j*hight+y,number);
+			}
+		 }
+		 
+		 //draw selected area
+		 Paint selected = new Paint();
+	 	 selected.setColor(Color.CYAN);
+	 	 selected.setStrokeWidth(6);
+	 	 selected.setStyle(Paint.Style.STROKE); 
+	 	 if(!game.isOriginal(selectX, selectY)){
+	 		  canvas.drawRect(selectRect, selected);
+	 	 }
+	 	
+	 	 
+	 	 //to check if it is duplicate
+		 Paint errorline = new Paint();
+		 errorline.setColor(Color.RED);
+		 errorline.setStrokeWidth(6);
+		 errorline.setStyle(Paint.Style.STROKE); 
+	 	 if(isFill==true){
+	 		if(game.isDuplicate9(selectX, selectY,game.i)){
+	 			if(!game.isOriginal(selectX, selectY)){
+	 				canvas.drawRect(duplicatetRect, errorline);
+					Log.d(" ", "false");
+	 			}
+		 	 }
+		 	 else {
+		 		Log.d(" ", "t");
+			}
+	 	 }
+	 	 /*if(game.isDuplicate9(selectX, selectY,game.i)){
+			 canvas.drawRect(duplicatetRect, errorline);
+			 Log.d(" ", "false");
+	 	 }
+	 	 else {
+	 		Log.d(" ", "t");
+		}*/
+	 	 
+	 	 if(game.isFinished()){
+	 		 if(game.isRight()){
+	 			 
+	 		 }
+	 	 }
+	 	 
+	 	 //showNumber(selectX, selectY);
+
+		 //canvas.drawRect(selRect, selected); 
+//		 while(i<81){
+//			if(selected==true){
+//		 		Paint selected = new Paint();
+//		 		selected.setColor(Color.CYAN);
+//		 		selected.setStrokeWidth(6);
+//		 		selected.setStyle(Paint.Style.STROKE); 
+//		 		canvas.drawRect(13 + selectX*width,10+selectY*hight,13+(selectX+1)*width, 10+(selectY+1)*hight,selected);
+//		 		i++;		 		
+//		 	}
+//			selected=false;
+//		 }
+
+	 	 //area of selecting numbers  
+//	 	 for(int i=0;i<9;i++){
+//	 		 canvas.drawRect(13+i*width, getHeight()-425, 13+(i+1)*width,getHeight()-425+width, broaderline);
+//	 	 }
+//		 String[] num=new String[9];
+//		 num[0]="1";
+//		 num[1]="2";
+//		 num[2]="3";
+//		 num[3]="4";
+//		 num[4]="5";
+//		 num[5]="6";
+//		 num[6]="7";
+//		 num[7]="8";
+//		 num[8]="9";
+//	 	
+//		 for(int i=0;i<9;i++){
+//			 	
+//				canvas.drawText(num[i],i*width+x,getHeight()-425+y,number);
+//			}
+		 
+	 	 	 	 
+		super.onDraw(canvas); 
+		 
+	 }
+	 
+	 @Override
+	public boolean onTouchEvent(MotionEvent event) {
+		 float clickX=event.getX();
+		 float clickY=event.getY();
+		 selectX = (int) ((clickX-13)/width);
+		 selectY = (int) ((clickY-10)/width);
+		 
+		 if (event.getAction()!= MotionEvent.ACTION_DOWN)
+			 return super.onTouchEvent(event);	
+		 Log.d(String.valueOf(selectX) , String.valueOf(selectY) );
+		 Log.d("hhh", "aa");//to check if click successfully		 
+		 if(clickY>10&&clickY<getWidth()-16)
+			 selectedR(selectX, selectY);
+		 	 duplicateR(selectX, selectY);
+		 //initiaNum(selectX,selectY);
+		 //game.now[selectX][selectX] = game.i;
+		 isFill=game.fill_in_blank(game.i, selectX, selectY);
+		 Log.d("array", game.getString(selectX, selectY));
+		 //showNumber(selectX, selectY);
+
+
+		 
+//		 Canvas s = null;
+//		 Paint selected = new Paint();
+//		 selected.setColor(Color.CYAN);
+//		 selected.setStrokeWidth(6);
+//		 selected.setStyle(Paint.Style.STROKE); 
+//		 s.drawRect(13 + selectX*width,10+selectY*hight,13+(selectX+1)*width, 10+(selectY+1)*hight,selected);
+//		 		 
+		return true;
+	}
+	 
+
+	 
+//		private void initiaNum(int x, int y) {
+//			
+//			
+//			invalidate();
+//	}
+
+		public void selectedR(int x,int y){
+			invalidate(selectRect);
+			selectRect.set((int)(13+x*width), (int)(10+y*hight), (int)(13+(x+1)*width),(int)(13+(y+1)*hight));
+			invalidate(selectRect);
+		}
+		
+		public void duplicateR(int x,int y){
+			
+			duplicatetRect.set((int)(13+x*width), (int)(10+y*hight), (int)(13+(x+1)*width),(int)(13+(y+1)*hight));
+			invalidate(duplicatetRect);
+		}
+		
+//		public void showNumber(int x,int y){
+//				game.setNum(x, y);
+//				invalidate();
+//						
+//		}
+
+
+	
+
+}
